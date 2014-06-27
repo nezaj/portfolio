967aaf085af72a835d0104a74981bc65230b1f3c
Joe Averbukh <joeaverbukh@gmail.com>
2014-06-26 18:57:21 -0700

    Add JS to center window horiz and vert

--- src/assets.py
+++ src/assets.py
@@ -1,8 +1,11 @@
 from flask_assets import Environment, Bundle
 
 CSS_ASSETS = [
-    'css/vendor/bootstrap.css',
     Bundle('css/application.scss', filters='pyscss', output='css/compiled-scss.css')
+]
+
+JS_ASSETS = [
+    'js/centered.js'
 ]
 
 def register_assets(app):
@@ -12,7 +15,9 @@ def register_assets(app):
     assets.url = app.static_url_path
 
     css_all = Bundle(*CSS_ASSETS, filters='cssmin', output='css/bundle.min.css')
+    js_all = Bundle(*JS_ASSETS, filters='rjsmin', output='js/all.min.js')
 
     assets.register('css_all', css_all)
+    assets.register('js_all', js_all)
     app.logger.info("Registered assets...")
     return assets
