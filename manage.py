cfe6ed62a488bf9a30ae233237f869707778c3ea
Joe Averbukh <joeaverbukh@gmail.com>
2014-06-26 20:29:35 -0700

    Random fixes

--- manage.py
+++ manage.py
@@ -3,14 +3,6 @@
 from flask_script import Manager
 from flask_script.commands import ShowUrls
 
-from src.app import create_app
-from src.config import app_config
-
-flask_app = create_app(app_config)
-manager = Manager(flask_app)
-
-manager.add_command("routes", ShowUrls())
-
 def import_env():
     import os
     if os.path.exists('.env'):
@@ -22,4 +14,12 @@ def import_env():
 
 if __name__ == '__main__':
     import_env()
+
+    from src.app import create_app
+    from src.config import app_config
+
+    flask_app = create_app(app_config)
+    manager = Manager(flask_app)
+    manager.add_command("routes", ShowUrls())
+
     manager.run()
