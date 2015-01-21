79022001c72b1e0bdb031bd3e4c97073d3115752
Joe Averbukh <joeaverbukh@gmail.com>
2015-01-21 14:23:05 -0800

    Minify jquery and tipsy

--- src/assets.py
+++ src/assets.py
@@ -1,7 +1,14 @@
 from flask_assets import Environment, Bundle
 
 CSS_ASSETS = [
+    'css/vendor/tipsy.css',
     Bundle('css/application.scss', filters='pyscss', output='css/compiled-scss.css')
+]
+
+JS_ASSETS = [
+    'js/vendor/jquery.js',
+    'js/vendor/tipsy.js',
+    'js/application.js'
 ]
 
 def register_assets(app):
@@ -11,7 +18,9 @@ def register_assets(app):
     assets.url = app.static_url_path
 
     css_all = Bundle(*CSS_ASSETS, filters='cssmin', output='css/bundle.min.css')
+    js_all = Bundle(*JS_ASSETS, filters='rjsmin', output='js/all.min.js')
 
     assets.register('css_all', css_all)
+    assets.register('js_all', js_all)
     app.logger.info("Registered assets...")
     return assets
