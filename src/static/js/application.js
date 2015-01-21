cf5cd1547f45151ec590e29eef4374fa0c29fabe
Joe Averbukh <joeaverbukh@gmail.com>
2015-01-21 14:41:21 -0800

    Only apply tooltips for desktop devices

--- src/static/js/application.js
+++ src/static/js/application.js
@@ -1,5 +1,9 @@
 // Application-wide js
 
 $(function(){
-  $('a[rel=tipsy]').tipsy({gravity: 's'});
+  // Only apply tooltips for Desktop devices
+  var mq = window.matchMedia( "(min-width: 992px)" );
+  if (mq.matches) {
+    $('a[rel=tipsy]').tipsy({gravity: 's'})
+  }
 });
